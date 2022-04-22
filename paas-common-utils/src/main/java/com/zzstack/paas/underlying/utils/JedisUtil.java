6814e6852d69c8162c61a874ce5ab2e38f514343
maoge <ultravirs@outlook.com>
2022-04-22 16:59:11 +0800

    jedis 3.4.1 -> 4.2.2

--- paas-common-utils/src/main/java/com/zzstack/paas/underlying/utils/JedisUtil.java
+++ paas-common-utils/src/main/java/com/zzstack/paas/underlying/utils/JedisUtil.java
@@ -1,13 +1,17 @@
 package com.zzstack.paas.underlying.utils;
 
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import redis.clients.jedis.*;
 
 public class JedisUtil {
 
     private static final String REDIS_ADDR_SPLIT = ",";
+    private static int MAX_ATTEMPTS = 5;
     private static int REDIS_CONN_POOL_MAX = 20;
     private static int REDIS_CONN_POOL_MIN = 10;
     private static long REDIS_MAX_WAIT_MILLIS = 3000;
@@ -23,16 +27,43 @@ public class JedisUtil {
 			nodes.add(hostAndPort);
 		}
 
-		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		GenericObjectPoolConfig<Connection> poolConfig = new GenericObjectPoolConfig<Connection>();
 		poolConfig.setMaxTotal(REDIS_CONN_POOL_MAX);
+		poolConfig.setMinIdle(REDIS_CONN_POOL_MIN);
 		poolConfig.setMaxIdle(REDIS_CONN_POOL_MIN);
-		poolConfig.setMaxWaitMillis(REDIS_MAX_WAIT_MILLIS);
-		poolConfig.setTestOnBorrow(true);
+		poolConfig.setMaxWait(Duration.ofMillis(REDIS_MAX_WAIT_MILLIS));
+		poolConfig.setTestOnBorrow(false);
 		poolConfig.setTestOnReturn(false);
 
 		return new JedisCluster(nodes, poolConfig);
 	}
+	
+    public static JedisCluster getPool(String clusterAddr, String auth, int maxPoolSize, int minPoolSize, int maxWaitMills) {
+        if (clusterAddr == null || clusterAddr.isEmpty())
+            return null;
 
+        Set<HostAndPort> nodes = new HashSet<>();
+        String[] addrArr = clusterAddr.split(REDIS_ADDR_SPLIT);
+        for (String addr : addrArr) {
+            HostAndPort hostAndPort = parseAddr(addr);
+            nodes.add(hostAndPort);
+        }
+
+        GenericObjectPoolConfig<Connection> poolConfig = new GenericObjectPoolConfig<Connection>();
+        poolConfig.setMaxTotal(maxPoolSize);
+        poolConfig.setMinIdle(minPoolSize);
+        poolConfig.setMaxIdle(minPoolSize);
+        poolConfig.setMaxWait(Duration.ofMillis(maxWaitMills));
+        poolConfig.setTestOnBorrow(false);
+        poolConfig.setTestOnReturn(false);
+
+        if (auth != null && !auth.isEmpty()) {
+            return new JedisCluster(nodes, maxWaitMills, maxWaitMills, MAX_ATTEMPTS, auth, poolConfig);
+        } else {
+            return new JedisCluster(nodes, maxWaitMills, maxWaitMills, MAX_ATTEMPTS, poolConfig);
+        }
+    }
+	
 	public static Jedis getPool(String host, int port) {
 	    return new Jedis(host, port);
 	}
