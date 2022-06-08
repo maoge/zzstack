95e58aa17b0ff1802f0d9da130c755341d5a108b
maoge <ultravirs@outlook.com>
2022-06-08 23:26:16 +0800

    支持外部引用时，指定数据源来获取seq id

--- paas-common-dbclient/src/main/java/com/zzstack/paas/underlying/dbclient/utils/SequenceId.java
+++ paas-common-dbclient/src/main/java/com/zzstack/paas/underlying/dbclient/utils/SequenceId.java
@@ -73,11 +73,60 @@ public class SequenceId {
         return id;
     }
     
+    public long getNextId(String dbName, String seqName) throws SeqException {
+        LongMargin margin = seqMap.get(seqName);
+        if (margin == null) {
+            margin = getNextSeqMargin(dbName, seqName, DEFAULT_SEQ_STEP);
+            if (margin != null) {
+                seqMap.put(seqName, margin);
+            } else {
+                String info = String.format("%s %s", seqName, DBConsts.ERR_SEQ_NOT_EXISTS);
+                throw new SeqException(info);
+            }
+        }
+        
+        long id = margin.getNextId();
+        if (id == -1) {
+            // id取完了，拉取下一步长
+            margin = getNextSeqMargin(dbName, seqName, DEFAULT_SEQ_STEP);
+            id = margin.getNextId();
+        }
+        
+        return id;
+    }
+    
     public static LongMargin getNextSeqMargin(String seqName, int step) {
         LongMargin ret = null;
         
         try {
             CRUD c = new CRUD();
+            
+            SqlBean sqlBean1 = new SqlBean(DBConstants.SQL_NEXT_SEQ_LOCK);
+            sqlBean1.addParams(new Object[] { seqName });
+            c.putSqlBean(sqlBean1);
+            
+            String sql2 = String.format(DBConstants.SQL_NEXT_SEQ_UPDATE, step);
+            SqlBean sqlBean2 = new SqlBean(sql2);
+            sqlBean2.addParams(new Object[] { seqName });
+            c.putSqlBean(sqlBean2);
+    
+            ResultBean result = new ResultBean();
+            ret = c.getNextSeqMargin(step, result);
+            if (result.getRetCode() == CONSTS.REVOKE_NOK)
+                return null;
+            
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        
+        return ret;
+    }
+
+    public static LongMargin getNextSeqMargin(String dbName, String seqName, int step) {
+        LongMargin ret = null;
+        
+        try {
+            CRUD c = new CRUD(dbName);
             
             SqlBean sqlBean1 = new SqlBean(DBConstants.SQL_NEXT_SEQ_LOCK);
             sqlBean1.addParams(new Object[] { seqName });
